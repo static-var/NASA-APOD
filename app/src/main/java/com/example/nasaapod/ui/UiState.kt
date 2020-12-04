5a1bd4eae49246bce08619d8362df0d50d028d73
“UnusedVariable” <slodha96@gmail.com>
2020-12-04 23:49:55 +0530

    Add sealed class to represent UI state

---
+++ app/src/main/java/com/example/nasaapod/ui/UiState.kt
@@ -0,0 +1,7 @@
+package com.example.nasaapod.ui
+
+sealed class UiState<T>
+
+data class Loading<T>(private val showLoader: Boolean = true) : UiState<T>()
+data class Error<T>(val errorMessage: String = "") : UiState<T>()
+data class Success<T>(val data: T) : UiState<T>()
