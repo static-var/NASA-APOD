c840d4d9e14c6fc9978af54b2e1699425555793e
“UnusedVariable” <slodha96@gmail.com>
2020-12-04 23:55:04 +0530

    Add json adapter for list of ApodImage to moshi module

--- app/src/main/java/com/example/nasaapod/di/MoshiModule.kt
+++ app/src/main/java/com/example/nasaapod/di/MoshiModule.kt
@@ -1,6 +1,9 @@
 package com.example.nasaapod.di
 
+import com.example.nasaapod.data.model.ApodImage
+import com.squareup.moshi.JsonAdapter
 import com.squareup.moshi.Moshi
+import com.squareup.moshi.Types
 import com.squareup.moshi.kotlin.reflect.KotlinJsonAdapterFactory
 import dagger.Module
 import dagger.Provides
@@ -18,5 +21,12 @@ object MoshiModule {
         return Moshi.Builder()
             .add(KotlinJsonAdapterFactory())
             .build()
+    }
+
+    @Provides
+    @Singleton
+    fun provideApodImageListAdapter(moshi: Moshi): JsonAdapter<List<ApodImage>> {
+        val typeOfApodImageList = Types.newParameterizedType(List::class.java, ApodImage::class.java)
+        return moshi.adapter<List<ApodImage>>(typeOfApodImageList)
     }
 }
