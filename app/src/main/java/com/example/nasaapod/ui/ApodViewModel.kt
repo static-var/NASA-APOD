5b88c9ea93e99e4d5fe1da263f29d6833f8bc6fc
“UnusedVariable” <slodha96@gmail.com>
2020-12-04 21:06:18 +0530

    Create initial viewmodel
    Signed-off-by: “UnusedVariable” <slodha96@gmail.com>

---
+++ app/src/main/java/com/example/nasaapod/ui/ApodViewModel.kt
@@ -0,0 +1,7 @@
+package com.example.nasaapod.ui
+
+import androidx.hilt.lifecycle.ViewModelInject
+import androidx.lifecycle.ViewModel
+
+class ApodViewModel @ViewModelInject constructor(): ViewModel() {
+}
