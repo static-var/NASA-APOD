67a14b55223a48a81b85facf216dcd9cefd0a6af
“UnusedVariable” <slodha96@gmail.com>
2020-12-04 21:06:42 +0530

    Attach viewmodel to main activity

--- app/src/main/java/com/example/nasaapod/MainActivity.kt
+++ app/src/main/java/com/example/nasaapod/MainActivity.kt
@@ -1,11 +1,15 @@
 package com.example.nasaapod
 
 import android.os.Bundle
+import androidx.activity.viewModels
 import androidx.appcompat.app.AppCompatActivity
+import com.example.nasaapod.ui.ApodViewModel
 import dagger.hilt.android.AndroidEntryPoint
 
 @AndroidEntryPoint
 class MainActivity : AppCompatActivity() {
+
+    private val viewModel: ApodViewModel by viewModels()
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
