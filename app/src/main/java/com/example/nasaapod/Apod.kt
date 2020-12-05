477698df5d7754f237a8fadc2e9abfb3808efa33
“UnusedVariable” <slodha96@gmail.com>
2020-12-05 22:36:36 +0530

    Initialize ThreeTenABP in application class

--- app/src/main/java/com/example/nasaapod/Apod.kt
+++ app/src/main/java/com/example/nasaapod/Apod.kt
@@ -1,7 +1,13 @@
 package com.example.nasaapod
 
 import android.app.Application
+import com.jakewharton.threetenabp.AndroidThreeTen
 import dagger.hilt.android.HiltAndroidApp
 
 @HiltAndroidApp
-class Apod: Application() { }
+class Apod: Application() {
+    override fun onCreate() {
+        super.onCreate()
+        AndroidThreeTen.init(this)
+    }
+}
