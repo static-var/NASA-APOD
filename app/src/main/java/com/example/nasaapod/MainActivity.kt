aaeac7cf6db0b96bcbcb3ae56148833a92eb6525
“UnusedVariable” <slodha96@gmail.com>
2020-12-05 22:41:44 +0530

    Connect action bar with navigation components

--- app/src/main/java/com/example/nasaapod/MainActivity.kt
+++ app/src/main/java/com/example/nasaapod/MainActivity.kt
@@ -3,15 +3,28 @@ package com.example.nasaapod
 import android.os.Bundle
 import androidx.activity.viewModels
 import androidx.appcompat.app.AppCompatActivity
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.ui.setupActionBarWithNavController
+import com.example.nasaapod.databinding.ActivityMainBinding
 import com.example.nasaapod.ui.ApodViewModel
 import dagger.hilt.android.AndroidEntryPoint
 
 @AndroidEntryPoint
 class MainActivity : AppCompatActivity() {
 
+    private lateinit var viewBinding: ActivityMainBinding
     private val viewModel by viewModels<ApodViewModel>()
+
+    private val navHostFragment by lazy { supportFragmentManager.findFragmentById(R.id.fragment_container) as NavHostFragment }
+    private val navController by lazy { navHostFragment.navController }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        viewBinding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(viewBinding.root)
+
+        setupActionBarWithNavController(navController)
     }
+
+    override fun onSupportNavigateUp() = navController.navigateUp()
 }
