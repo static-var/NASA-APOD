e65b5ce9097a3a72db95c68186a969dbc25a9b76
“UnusedVariable” <slodha96@gmail.com>
2020-12-05 22:50:35 +0530

    viewModel: Keep a copy of list of data coming from repository and emit loading state first to UI while fetching data from repository

--- app/src/main/java/com/example/nasaapod/ui/ApodViewModel.kt
+++ app/src/main/java/com/example/nasaapod/ui/ApodViewModel.kt
@@ -5,6 +5,7 @@ import androidx.lifecycle.LiveData
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.asLiveData
 import androidx.lifecycle.liveData
+import com.example.nasaapod.data.model.ApodImage
 import com.example.nasaapod.data.repository.ApodRepository
 import com.example.nasaapod.di.IoDispatcher
 import kotlinx.coroutines.CoroutineDispatcher
@@ -15,6 +16,16 @@ class ApodViewModel @ViewModelInject constructor(
 ) : ViewModel() {
 
     /**
+     * This will hold a saved copy of list of [ApodImage] fetched from repository layer.
+     */
+    var apodImages: List<ApodImage> = listOf()
+        private set
+
+    fun setApodImages(data: List<ApodImage>) {
+        apodImages = data
+    }
+
+    /**
      * Function calls [ApodRepository.getApodList] and converts its to a LiveData
      * Reason why [SafeFlow] was converted to [LiveData]
      * [reason](https://developer.android.com/kotlin/flow/stateflow-and-sharedflow)
@@ -22,6 +33,7 @@ class ApodViewModel @ViewModelInject constructor(
      * @return LiveData of UiState which holds either List of ApodImage or an error message
      */
     fun getApodImageList() = liveData {
+        emit(Loading())
         emitSource(apodRepository.getApodList().asLiveData(ioDispatcher))
     }
 
