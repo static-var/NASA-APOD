ff3f1fd56058f7906a303a3ff1cb78edcceb7f9b
“UnusedVariable” <slodha96@gmail.com>
2020-12-06 17:33:51 +0530

    moshi module: Make adapter null safe, and remove kotlinJsonAdapterFactory

--- app/src/main/java/com/example/nasaapod/di/MoshiModule.kt
+++ app/src/main/java/com/example/nasaapod/di/MoshiModule.kt
@@ -4,7 +4,6 @@ import com.example.nasaapod.data.model.ApodImage
 import com.squareup.moshi.JsonAdapter
 import com.squareup.moshi.Moshi
 import com.squareup.moshi.Types
-import com.squareup.moshi.kotlin.reflect.KotlinJsonAdapterFactory
 import dagger.Module
 import dagger.Provides
 import dagger.hilt.InstallIn
@@ -19,14 +18,14 @@ object MoshiModule {
     @Singleton
     fun provideMoshi(): Moshi {
         return Moshi.Builder()
-            .add(KotlinJsonAdapterFactory())
             .build()
     }
 
     @Provides
     @Singleton
     fun provideApodImageListAdapter(moshi: Moshi): JsonAdapter<List<ApodImage>> {
-        val typeOfApodImageList = Types.newParameterizedType(List::class.java, ApodImage::class.java)
-        return moshi.adapter<List<ApodImage>>(typeOfApodImageList)
+        val typeOfApodImageList =
+            Types.newParameterizedType(List::class.java, ApodImage::class.java)
+        return moshi.adapter<List<ApodImage>>(typeOfApodImageList).nullSafe()
     }
 }
